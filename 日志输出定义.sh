4c4920907934580315a20aec6ec5f2f1dd604a9e
Outsrkem <xhdascnf@126.com>
2020-12-12 11:22:27 +0800

    shell 日志定义及输出

--- 日志输出定义.sh
+++ 日志输出定义.sh
@@ -14,5 +14,24 @@ function log_warn() {
     echo "[`date +%Y-%m-%d\ %H:%M:%S`] [W] $@"  >> $LogFile
 }
 log_info "[系统资源信息]"
-log_error "[系统资源信息]"
 log_warn "[系统资源信息]"
+log_error "[系统资源信息]"
+
+
+#############################################################################
+#!/bin/bash
+function __log() {
+	if [ "$1" == "info" ];then
+		echo -e "\033[32m [INFO]  \033[0m${@:2} "
+	elif [ "$1" == "warn" ];then
+		echo -e "\033[33m [WARN]  \033[0m${@:2} "
+	elif [ "$1" == "error" ];then
+		echo -e "\033[31m [ERROR] \033[0m${@:2} "
+	fi
+
+}
+
+
+__log info "asdasdasd"
+__log warn "asdasdasd"
+__log error "asdasdasd"
