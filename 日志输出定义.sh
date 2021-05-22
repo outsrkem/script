e5db1874111efeab7afeee78fa42a3a4aa9a4bae
Outsrkem <xhdascnf@126.com>
2021-05-22 15:16:06 +0800

    优化shell定义的日志格式

--- 日志输出定义.sh
+++ 日志输出定义.sh
@@ -1,33 +1,33 @@
 #!/bin/bash
 LogFile=/tmp/log
 > $LogFile
-function log_error() {
-    echo -e "\033[31m [`date +%Y-%m-%d\ %H:%M:%S`] [E] $@ \033[0m"
+function __log_error() {
+    echo -e "\033[31m[`date +%Y-%m-%d\ %H:%M:%S`][E]\033[0m $@"
     echo "[`date +%Y-%m-%d\ %H:%M:%S`] [E] $@"  >> $LogFile
 }
-function log_info() {
-    echo -e "\033[32m [`date +%Y-%m-%d\ %H:%M:%S`] [I] $@ \033[0m"
+function __log_info() {
+    echo -e "\033[32m[`date +%Y-%m-%d\ %H:%M:%S`][I]\033[0m $@"
     echo "[`date +%Y-%m-%d\ %H:%M:%S`] [I] $@"  >> $LogFile
 }
-function log_warn() {
-    echo -e "\033[33m [`date +%Y-%m-%d\ %H:%M:%S`] [W] $@ \033[0m"
+function __log_warn() {
+    echo -e "\033[33m[`date +%Y-%m-%d\ %H:%M:%S`][W]\033[0m $@"
     echo "[`date +%Y-%m-%d\ %H:%M:%S`] [W] $@"  >> $LogFile
 }
-log_info "[系统资源信息]"
-log_warn "[系统资源信息]"
-log_error "[系统资源信息]"
+__log_info "[$LINENO][系统资源信息]"
+__log_warn "[$LINENO][系统资源信息]"
+__log_error "[$LINENO][系统资源信息]"
 
 
 #############################################################################
 #!/bin/bash
 function __log() {
-	if [ "$1" == "info" ];then
-		echo -e "\033[32m [INFO]  \033[0m${@:2} "
-	elif [ "$1" == "warn" ];then
-		echo -e "\033[33m [WARN]  \033[0m${@:2} "
-	elif [ "$1" == "error" ];then
-		echo -e "\033[31m [ERROR] \033[0m${@:2} "
-	fi
+  if [ "$1" == "info" ];then
+    echo -e "\033[32m [INFO]  \033[0m${@:2} "
+  elif [ "$1" == "warn" ];then
+    echo -e "\033[33m [WARN]  \033[0m${@:2} "
+  elif [ "$1" == "error" ];then
+    echo -e "\033[31m [ERROR] \033[0m${@:2} "
+  fi
 
 }
 
