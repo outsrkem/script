3d77270b3c2ac419b376dc6ddb15d16d6cea2332
Outsrkem <xhdascnf@126.com>
2021-07-18 10:50:43 +0800

    优化系统初始化脚本

--- initializeCentos7.sh
+++ initializeCentos7.sh
@@ -10,16 +10,21 @@ yum clean all && yum makecache
 echo "export PS1='\[\e[1;32m\][\u@\h \W]\\$ \[\e[0m\]'" >> /etc/bashrc
 echo "export TIME_STYLE='+%Y-%m-%d %H:%M:%S'" >> /etc/bashrc
 echo "export HISTTIMEFORMAT='%F %T  '" >> /etc/profile
+
+
 sed -i "s/^#UseDNS.*/UseDNS no/g" /etc/ssh/sshd_config 
 sed -i "s/SELINUX=.*/SELINUX=disabled/g" /etc/selinux/config
 systemctl stop firewalld && systemctl disable firewalld && rpm -e --nodeps firewalld
-systemctl disable chronyd 
+systemctl disable chronyd
 systemctl stop chronyd
 yum -y install iptables-services
 systemctl start iptables && iptables -F && service iptables save
-yum -y install lrzsz net-tools bash-completion vim tree wget dos2unix ntpdate unzip psmisc  kernel-tools  tcpdump 
+yum -y install lrzsz net-tools bash-completion vim tree wget dos2unix ntpdate unzip psmisc kernel-tools tcpdump mailx patch
+
 cat >> ~/.vimrc <<EOF
 set ts=4
-set expandtab
-set paste
 EOF
+
+# 配置私有CA
+curl -sSo /etc/pki/ca-trust/source/anchors/private-ca-root.pem \
+  https://gitee.com/outsrkem/my_private_ca_pem/raw/main/pki/ca.pem && /bin/update-ca-trust
