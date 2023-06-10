5e5ba20e96e53cb21f3999e0374c5d21bc2cd5d7
outsrkem <xhdascnf@126.com>
2023-06-10 10:24:24 +0800

    优化脚本

--- shell/日志输出定义.sh
+++ shell/日志输出定义.sh
@@ -1,37 +1,22 @@
 #!/bin/bash
-LogFile=/tmp/log
-> $LogFile
+log_file=/tmp/log
 function __log_error() {
-    echo -e "\033[31m[`date +%Y-%m-%d\ %H:%M:%S`][E]\033[0m $@"
-    echo "[`date +%Y-%m-%d\ %H:%M:%S`] [E] $@"  >> $LogFile
+    local log_time=`date "+[%F %T %z]"`
+    echo -e "\033[31m${log_time} [E]\033[0m $@"
+    echo "${log_time} [E] $@"  >> $log_file
 }
 function __log_info() {
-    echo -e "\033[32m[`date +%Y-%m-%d\ %H:%M:%S`][I]\033[0m $@"
-    echo "[`date +%Y-%m-%d\ %H:%M:%S`] [I] $@"  >> $LogFile
+    local log_time=`date "+[%F %T %z]"`
+    echo -e "\033[32m${log_time} [I]\033[0m $@"
+    echo "${log_time} [I] $@"  >> $log_file
 }
 function __log_warn() {
-    echo -e "\033[33m[`date +%Y-%m-%d\ %H:%M:%S`][W]\033[0m $@"
-    echo "[`date +%Y-%m-%d\ %H:%M:%S`] [W] $@"  >> $LogFile
+    local log_time=`date "+[%F %T %z]"`
+    echo -e "\033[33m${log_time} [W]\033[0m $@"
+    echo "${log_time} [W] $@"  >> $log_file
 }
+
 __log_info "[$LINENO][系统资源信息]"
 __log_warn "[$LINENO][系统资源信息]"
 __log_error "[$LINENO][系统资源信息]"
 
-
-#############################################################################
-#!/bin/bash
-function __log() {
-  if [ "$1" == "info" ];then
-    echo -e "\033[32m [INFO]  \033[0m${@:2} "
-  elif [ "$1" == "warn" ];then
-    echo -e "\033[33m [WARN]  \033[0m${@:2} "
-  elif [ "$1" == "error" ];then
-    echo -e "\033[31m [ERROR] \033[0m${@:2} "
-  fi
-
-}
-
-
-__log info "asdasdasd"
-__log warn "asdasdasd"
-__log error "asdasdasd"
