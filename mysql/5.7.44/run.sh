bc88e93769ef58616ece1e3e6d06db63362d5e24
outsrkem <xhdascnf@126.com>
2024-11-10 10:37:10 +0800

    优化mysql文档

--- mysql/5.7.44/run.sh
+++ mysql/5.7.44/run.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 docker run --name mysqld --restart=always \
 -v /etc/localtime:/etc/localtime:ro \
--v /usr/share/zoneinfo/iso3166.tab:/usr/share/zoneinfo/iso3166.tab:ro \
 -v /opt/mysqld/conf:/etc/mysql/mysql.conf.d \
 -v /opt/mysqld/data:/var/lib/mysql \
 -p 0.0.0.0:3306:3306/tcp \
--e MYSQL_ROOT_PASSWORD="123456@root##" \
--e TZ=Asia/Shanghai \
+-e MYSQL_ROOT_PASSWORD="root@pwd" \
 -d mysql:5.7.44
