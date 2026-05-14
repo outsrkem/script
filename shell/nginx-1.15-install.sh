e88edda95423768e9db1f38b0773442a46b8b7a4
Lark <lark.email@lark.com>
2026-05-14 21:54:06 +0800

    添加nginx源码编译指南

--- shell/nginx-1.15-install.sh
+++ shell/nginx-1.15-install.sh
@@ -93,4 +93,79 @@ GeoIP-devel GeoIP-update
 
 
 
+# 与rpm模块一模一样功能的编译配置
+yum install -y gcc gcc-c++ make \
+    pcre pcre-devel \
+    openssl openssl-devel \
+    zlib zlib-devel \
+    libxslt libxslt-devel \
+    gd gd-devel \
+    perl perl-devel \
+    perl-ExtUtils-Embed \
+    perl-IPC-Cmd
 
+# 1.下载openssl
+wget https://www.openssl.org/source/openssl-3.0.12.tar.gz
+tar xf openssl-3.0.12.tar.gz -C /usr/local/src
+
+# 2.编译Nginx
+./configure \
+--prefix=/usr/share/nginx \
+--sbin-path=/usr/sbin/nginx \
+--modules-path=/usr/lib64/nginx/modules \
+--conf-path=/etc/nginx/nginx.conf \
+--error-log-path=/var/log/nginx/error.log \
+--http-log-path=/var/log/nginx/access.log \
+--http-client-body-temp-path=/var/cache/nginx/client_body \
+--http-proxy-temp-path=/var/cache/nginx/proxy \
+--http-fastcgi-temp-path=/var/cache/nginx/fastcgi \
+--http-uwsgi-temp-path=/var/cache/nginx/uwsgi \
+--http-scgi-temp-path=/var/cache/nginx/scgi \
+--pid-path=/run/nginx.pid \
+--lock-path=/run/lock/subsys/nginx \
+--user=nginx \
+--group=nginx \
+--with-compat \
+--with-debug \
+--with-file-aio \
+--with-http_addition_module \
+--with-http_auth_request_module \
+--with-http_dav_module \
+--with-http_degradation_module \
+--with-http_flv_module \
+--with-http_gunzip_module \
+--with-http_gzip_static_module \
+--with-http_image_filter_module=dynamic \
+--with-http_mp4_module \
+--with-http_perl_module=dynamic \
+--with-http_random_index_module \
+--with-http_realip_module \
+--with-http_secure_link_module \
+--with-http_slice_module \
+--with-http_ssl_module \
+--with-http_stub_status_module \
+--with-http_sub_module \
+--with-http_v2_module \
+--with-http_xslt_module=dynamic \
+--with-mail=dynamic \
+--with-mail_ssl_module \
+--with-openssl=/usr/local/src/openssl-3.0.12 \
+--with-openssl-opt=enable-ktls \
+--with-pcre \
+--with-pcre-jit \
+--with-stream=dynamic \
+--with-stream_realip_module \
+--with-stream_ssl_module \
+--with-stream_ssl_preread_module \
+--with-threads
+
+
+make -j$(nproc)
+
+# 3.检查编译结果
+./objs/nginx -V
+nginx version: nginx/1.30.1
+built by gcc 4.8.5 20150623 (Red Hat 4.8.5-16) (GCC)
+built with OpenSSL 3.0.12 24 Oct 2023
+TLS SNI support enabled
+configure arguments: --prefix=/usr/share/nginx --sbin-path=/usr/sbin/nginx --modules-path=/usr/lib64/nginx/modules --conf-path=/etc/nginx/nginx.conf --error-log-path=/var/log/nginx/error.log --http-log-path=/var/log/nginx/access.log --http-client-body-temp-path=/var/cache/nginx/client_body --http-proxy-temp-path=/var/cache/nginx/proxy --http-fastcgi-temp-path=/var/cache/nginx/fastcgi --http-uwsgi-temp-path=/var/cache/nginx/uwsgi --http-scgi-temp-path=/var/cache/nginx/scgi --pid-path=/run/nginx.pid --lock-path=/run/lock/subsys/nginx --user=nginx --group=nginx --with-compat --with-debug --with-file-aio --with-http_addition_module --with-http_auth_request_module --with-http_dav_module --with-http_degradation_module --with-http_flv_module --with-http_gunzip_module --with-http_gzip_static_module --with-http_image_filter_module=dynamic --with-http_mp4_module --with-http_perl_module=dynamic --with-http_random_index_module --with-http_realip_module --with-http_secure_link_module --with-http_slice_module --with-http_ssl_module --with-http_stub_status_module --with-http_sub_module --with-http_v2_module --with-http_xslt_module=dynamic --with-mail=dynamic --with-mail_ssl_module --with-openssl=/usr/local/src/openssl-3.0.12 --with-openssl-opt=enable-ktls --with-pcre --with-pcre-jit --with-stream=dynamic --with-stream_realip_module --with-stream_ssl_module --with-stream_ssl_preread_module --with-threads
